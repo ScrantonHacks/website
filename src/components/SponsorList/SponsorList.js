5ae5e5d9a16949ccef2adcf0ee004282f99db67a
Andrew Plaza <andrew.plaza@wework.com>
2017-10-15 18:20:30 -0400

    three different sized images; now all we have to do is re-arrange them

--- src/components/SponsorList/SponsorList.js
+++ src/components/SponsorList/SponsorList.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import SponsorImage from '../SponsorImage';
 import Columns from 'grommet/components/Columns';
 
-const getSponsors = (sponsorArray) => {
-  return sponsorArray.map((sponsor)=>{
-      key += 1;
-      return (
-          <Avatar src={sponsor.logo} />
-      );
+const getSponsors = sponsorArray => {
+  let key = 0;
+  return sponsorArray.map( sponsor => {
+    key += 1;
+    return <SponsorImage logo = {__PATH_PREFIX__ + sponsor.logo} tier = {sponsor.tier} key={key} /> 
   });
 }
-export default ({sponsorArray}) => {
+
+
+const SponsorList = ({sponsors}) => {
+  console.log(sponsors);
   return (
-    <Columns justiy="center" responsive >
-        {getSponsors(sponsorArray)}
-    </Columns>
-    );
+    <div>
+      {getSponsors(sponsors)}
+    </div>
+  );
+};
+
+SponsorList.propTypes = {
+  sponsorArray: PropTypes.array.isRequired
 }
+
+SponsorList.defaultProps = {
+  sponsorArray: [{logo: 'default', tier: 1}],
+}
+
+export default SponsorList;
