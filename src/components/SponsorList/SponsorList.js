e4b4a4682049ae5f6243cf537c419f4707c318ef
Sean Batzel <romulus108@protonmail.com>
2017-10-15 15:58:04 -0400

    Alright, for whatever reason I'm getting TypeError: sponsorArray is undefined. So that's adorable.

--- src/components/SponsorList/SponsorList.js
+++ src/components/SponsorList/SponsorList.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import SponsorImage from '../SponsorImage';
+import Columns from 'grommet/components/Columns';
 
 const getSponsors = (sponsorArray) => {
   return sponsorArray.map((sponsor)=>{
-    
+      key += 1;
+      return (
+          <Avatar src={sponsor.logo} />
+      );
   });
 }
 export default ({sponsorArray}) => {
   return (
-    <Columns>
+    <Columns justiy="center" responsive >
+        {getSponsors(sponsorArray)}
     </Columns>
-  )
+    );
 }
-
-
-
